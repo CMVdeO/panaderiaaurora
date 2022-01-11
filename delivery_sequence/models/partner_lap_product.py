bc3e4d031c9f7db1e538b4c9b8f29bf8941220f0
Vicente <v.deorue01@ufromail.cl>
2022-01-11 16:43:48 -0300

    [FIX] Productos de vuelta solamente vendibles

--- delivery_sequence/models/partner_lap_product.py
+++ delivery_sequence/models/partner_lap_product.py
@@ -9,6 +9,6 @@ class PartnerLapProduct(models.Model):
 
 
     partner_lap_id = fields.Many2one("delivery_sequence.partner_lap", ondelete='cascade', string="Vuelta del cliente", required=True)
-    product_id = fields.Many2one("product.product", string="Product", required=True, domain="[('sale_ok', '=', False)]")
+    product_id = fields.Many2one("product.product", string="Product", required=True, domain="[('sale_ok', '=', True)]")
     quantity = fields.Float(required=True)
     #TODO: decimales en float... quantity = fields.Float(required=True, digits=(16, 4), default=0.0)
